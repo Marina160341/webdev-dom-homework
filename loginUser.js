0aa78ab31d17c8e8306813ecac9acde29bd217cc
Marina160341 <kemova-marina@rambler.ru>
2023-11-09 16:17:18 +0300

    911

---
+++ loginUser.js
@@ -0,0 +1,71 @@
+import {loginUs, setToken, token} from './api.js'
+<<<<<<< HEAD
+import {renderRegistr} from './registration.js' 
+=======
+>>>>>>> 77b2d899f185e538ec693e751837def1b99e8785
+
+export const renderLogin = ({resultGET}) => {
+const appElement=document.getElementById("app")
+const loginHTML =
+<<<<<<< HEAD
+`
+<h2>Страница входа</h2>
+    <div class="form-autorization">
+    <input type="text" id="login-input" placeholder="Логин" class="login-input">
+    <br>
+    <input type="password"  id="password-input" class="password-input" placeholder="Пароль">
+    </div>
+    <br>
+    <button id="login-button" class="login-button">Авторизация</button>
+    <button id="registration-button" class="login-button">Регистрация</button>
+    
+=======
+`<h2>Страница входа</h2>
+    <div>
+    <input type="text" id="login-input" placeholder="Логин">
+    <input type="password"  id="password-input" placeholder="Пароль">
+    </div>
+    <br>
+    <button id="login-button">Авторизация</button>
+    <a href="index.html" id="link-to-tasks"></a>
+>>>>>>> 77b2d899f185e538ec693e751837def1b99e8785
+    `
+appElement.innerHTML=loginHTML;
+
+const buttonElementLogin = document.getElementById("login-button");
+const loginInputElement = document.getElementById("login-input");
+const passwordInputElement = document.getElementById("password-input");
+<<<<<<< HEAD
+
+=======
+>>>>>>> 77b2d899f185e538ec693e751837def1b99e8785
+    buttonElementLogin.addEventListener("click", () =>{
+        loginUs(
+            {
+        login: loginInputElement.value,
+        password: passwordInputElement.value,
+            }
+        ).then((responseData)=>{
+        console.log(token);
+        setToken (responseData.user.token);
+
+        console.log(token);
+        }).then(()=>{
+            resultGET() 
+        })
+        }
+        )
+        
+}
+<<<<<<< HEAD
+const buttonElementReg =document.getElementById("registration-button");
+buttonElementReg.addEventListener("click", () =>{
+    renderRegistr ({resultGET})
+  
+})
+
+        
+=======
+
+>>>>>>> 77b2d899f185e538ec693e751837def1b99e8785
+
