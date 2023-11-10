cf02e555f539d9ede3dccf6cd73fad834ca380a8
Marina160341 <kemova-marina@rambler.ru>
2023-11-10 13:51:14 +0300

    1011

--- api.js
+++ api.js
@@ -1,10 +1,24 @@
+const  commentsURL ='https://wedev-api.sky.pro/api/v2/kemova-marina/comments'
+const  userURL ='https://wedev-api.sky.pro/api/user/login'
+const registrationURL = 'https://wedev-api.sky.pro/api/user'
+export let token;
+export const setToken = (newToken) =>{
+    token = newToken;
+   }
+
 export function getTodos (){
-     return fetch('https://wedev-api.sky.pro/api/v1/kemova-marina/comments/', {
+     return fetch(commentsURL, {
         method: "GET",
+        headers: {Authorization: `Bearer ${token}`}
         
     }).then((response) => {
-        
-        if (response.status === 200) {
+if (response.status===401)
+{
+    password=prompt('Введите верный пароль')
+    getTodos ();
+    throw new error('Введите пароль ещё раз');
+}
+         else if (response.status === 200) {
             return response.json()
         } else {
             throw new Error("Сервер упал")
@@ -13,12 +27,14 @@ export function getTodos (){
 
  }
     export function postTodos ({text, name}){
-    return fetch('https://wedev-api.sky.pro/api/v1/kemova-marina/comments/', {
+    return fetch(commentsURL, {
             method: "POST",
+            headers: {Authorization: `Bearer ${token}`},
             body: JSON.stringify({
                 text: text,
-                name: name
-            })
+                name: name,
+                })
+                
         }).then((response) => {
             if (response.status === 500) {
                 throw new Error("Сервер упал")
@@ -30,3 +46,39 @@ export function getTodos (){
             };
         })
     }
+    export function loginUs ({login, password}){
+        return fetch(userURL, {
+        method: "POST",
+        body: JSON.stringify({
+        login,
+        password,
+                            })
+                    }).then((response) => {
+                         
+        return response.json();
+                        
+                    })
+                }
+
+                export function registration ({login, password, name}){
+                    return fetch(registrationURL, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        
+                            login,
+                            name,
+                            password,
+                          })
+                                }).then((response) => {
+                                     
+                    return response.json();
+                                    
+                                })
+                            }
+    // export function deleteTodo({ id }) {
+    //     return fetch("https://wedev-api.sky.pro/api/v1/kemova-marina/comments/" + id, {
+    //       method: "DELETE",
+    //     }).then((response) => {
+    //       return response.json();
+    //     });
+    //   }
